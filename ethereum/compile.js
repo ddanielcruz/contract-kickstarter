5e107f643d6bc908d3c29b5ab1166baa814beff3
Daniel Cruz <danielcunha54@gmail.com>
2022-06-09 20:59:30 -0300

    Add compile script

---
+++ ethereum/compile.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const solc = require('solc')
+const fs = require('fs-extra')
+
+const buildPath = path.resolve(__dirname, 'build')
+fs.removeSync(buildPath)
+fs.ensureDirSync(buildPath)
+
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol')
+const source = fs.readFileSync(campaignPath, 'utf-8')
+const input = {
+  language: 'Solidity',
+  sources: {
+    'Campaign.sol': {
+      content: source
+    }
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      }
+    }
+  }
+}
+
+const output = JSON.parse(solc.compile(JSON.stringify(input))).contracts['Campaign.sol']
+for (let contract in output) {
+  fs.outputJSONSync(path.resolve(buildPath, `${contract}.json`), output[contract])
+}
